3317aea5fd0b67ebbbd2ac0c4dd56f8c6bf645c9
Nithiesh Naik <nithieshnaik7@gmail.com>
2024-03-29 03:29:15 +0530

    till 7

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const parseQuery = require('./queryParser');
+const readCSV = require('./csvReader');
+
+
+
+function evaluateCondition(row, clause) {
+    const { field, operator, value } = clause;
+    switch (operator) {
+        case '=': return row[field] === value;
+        case '!=': return row[field] !== value;
+        case '>': return row[field] > value;
+        case '<': return row[field] < value;
+        case '>=': return row[field] >= value;
+        case '<=': return row[field] <= value;
+        default: throw new Error(`Unsupported operator: ${operator}`);
+    }
+}
+
+module.exports = executeSELECTQuery;
